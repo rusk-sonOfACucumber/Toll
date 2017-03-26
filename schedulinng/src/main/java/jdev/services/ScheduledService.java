d31ba030940d44b2d63a59dfe5eb22d444902d0a
jdev <saturn900@gmail.com>
2017-03-26 13:46:54 +0700

    autowired - зачистка

--- schedulinng/src/main/java/jdev/services/ScheduledService.java
+++ schedulinng/src/main/java/jdev/services/ScheduledService.java
@@ -1,6 +1,5 @@
 package jdev.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
