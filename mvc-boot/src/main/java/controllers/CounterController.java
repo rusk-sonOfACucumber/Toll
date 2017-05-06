7e16ff0e0da8a621ee260e8e54adbbd05f20d75b
jdev <saturn900@gmail.com>
2017-05-06 08:42:35 +0700

    удаление неиспользуемого import

--- mvc-boot/src/main/java/controllers/CounterController.java
+++ mvc-boot/src/main/java/controllers/CounterController.java
@@ -4,7 +4,6 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import jd.domain.Country;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
