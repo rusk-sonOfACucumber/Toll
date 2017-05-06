cc0f9169d0b5eedafd263643e68bda9b51339135
jdev <saturn900@gmail.com>
2017-05-06 07:19:49 +0700

    циклический запросчик

--- mvc-rest-consumer/src/main/java/jd/rest/consume/RestConsumer.java
+++ mvc-rest-consumer/src/main/java/jd/rest/consume/RestConsumer.java
@@ -11,20 +11,31 @@ import java.net.URL;
  */
 public class RestConsumer {
 
-    public static void main(String... args) throws IOException {
+    public static void main(String... args) throws IOException, InterruptedException {
         // получаем строку JSON от REST
 //        String response = IOUtils.toString(new URL("http://services.groupkt.com/country/get/iso2code/RU"), "UTF8");
-        String response = IOUtils.toString(new URL("http://localhost:8080/counter?name=The+Who"), "UTF8");
-        System.out.println(response);
+        while (true) {
+            String response = IOUtils.toString(new URL("http://localhost:8080/counter?name=The+Who"), "UTF8");
+            System.out.println(response);
 
-        response = IOUtils.toString(new URL("http://localhost:8080/coords?location=lat56.4,lon34.6"), "UTF8");
-        System.out.println(response);
+            response = IOUtils.toString(new URL("http://localhost:8080/coords?location=lat56.4,lon34.6"), "UTF8");
+            System.out.println(response);
 
-        response = IOUtils.toString(new URL("http://localhost:8080/coords?location=lat56.4"), "UTF8");
-        System.out.println(response);
+            response = IOUtils.toString(new URL("http://localhost:8080/coords?location=lat56.4"), "UTF8");
+            System.out.println(response);
 
-        response = IOUtils.toString(new URL("http://localhost:8080/coords"), "UTF8");
-        System.out.println(response);
+            try {
+                response = IOUtils.toString(new URL("http://localhost:8080/coords"), "UTF8");
+                System.out.println(response);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+
+            } finally {
+                Thread.sleep(2000);
+                System.out.println("==================================================");
+            }
+
+        }
         // преобразуем строку JSON в объект
         /*ObjectMapper mapper = new ObjectMapper();
         Country country = mapper.readValue(response, Country.class);
