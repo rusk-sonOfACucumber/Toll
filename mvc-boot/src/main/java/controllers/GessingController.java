6aa7562ecaf4b9b013fb738f4d554b57221fc843
jdev <saturn900@gmail.com>
2017-05-08 17:39:39 +0700

    юнит-тест для GessingController

--- mvc-boot/src/main/java/controllers/GessingController.java
+++ mvc-boot/src/main/java/controllers/GessingController.java
@@ -14,7 +14,7 @@ public class GessingController {
     FortuneService fortuneService;
 
     @RequestMapping("/gess")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="Noname") String name, Model model) {
+    public String gess(@RequestParam(value="name", required=false, defaultValue="Noname") String name, Model model) {
         model.addAttribute("name", name);
         model.addAttribute("num", fortuneService.tryFortune());
         return "gessing";
