12cb6cf521ed2e5815c1676ae9822535db9f76c8
BodDen <Denis-Bodrov@mail.ru>
2021-02-05 22:42:37 +0500

    добавил decodeDTO

--- common/src/test/java/jdev/dto/PointDTOTest.java
+++ common/src/test/java/jdev/dto/PointDTOTest.java
@@ -1,14 +1,20 @@
 package jdev.dto;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
  * Created by jdev on 06.03.2017.
  */
 public class PointDTOTest {
+
+
+    private String Expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\"}";
+    private String autoId = "o567gfd";
 
     @Test
     public void toJson() throws Exception {
@@ -18,5 +24,12 @@ public class PointDTOTest {
         point.setAutoId("o567gfd");
         assertTrue(point.toJson().contains("\"lat\":56"));
         System.out.println(point.toJson());
+    }
+
+    @Test
+    public void decodeDto() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        PointDTO dto = mapper.readValue(Expected, PointDTO.class);
+        assertEquals (autoId, dto.getAutoId());
     }
 }
